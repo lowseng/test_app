6bee3a00417495c126d196c580c25bf25616aafc
Anthony Low <alowseng@gmail.com>
2016-02-04 15:22:36 +0000

    1 2 many n debugger

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204111948) do
+ActiveRecord::Schema.define(version: 20160204144201) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160204111948) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "remarks"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
