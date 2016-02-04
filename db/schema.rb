6f9db5a965d329ca7a462236fbd7a475a2b93118
Anthony Low <alowseng@gmail.com>
2016-02-04 11:24:34 +0000

    user created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203034446) do
+ActiveRecord::Schema.define(version: 20160204111948) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160203034446) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "remarks"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
