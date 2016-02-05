3f1137f797016965cdaddf8e089cdadc7ad627c5
Anthony Low <alowseng@gmail.com>
2016-02-05 04:26:15 +0000

    crete show action and gravater

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
   def create
-    
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
@@ -11,6 +11,24 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
 
   private
