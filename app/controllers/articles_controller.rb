6da958076cdc94e6fad6329c50cbf2769cc47f7a
Anthony Low <alowseng@gmail.com>
2016-02-06 03:12:01 +0000

    lecture 119

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController			
 
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
-  def index	
-	  @articles = Article.all
-  end	
-
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 2)
+  end
+  
   def new
 	  @article = Article.new
   end
@@ -16,7 +18,8 @@ class ArticlesController < ApplicationController
 
   def create		
 	  @article = Article.new(article_params) 
- 
+    @article.user = current_use
+    
     if @article.save		
       flash[:success]='Article was successfully created'
       redirect_to article_path(@article)
@@ -57,5 +60,12 @@ class ArticlesController < ApplicationController
 	def article_params #(method for whitelisting)	
 		params.require(:article).permit(:title, :description)
 	end	
+	
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own articles"
+      redirect_to root_path
+    end
+  end
 end			
 		
